b96c3d828937729e6609d1a3c35766672eb14797
vunnelsewhere <venuscychan14@gmail.com>
2023-07-31 15:50:57 +0800

    "Created Client Part"

---
+++ app/src/main/java/DataCache/DataCache.java
@@ -0,0 +1,9 @@
+package DataCache;
+
+/*
+ * A class that will store all the data for the application
+ * Once we've downloaded the family tree data from the server, we're going to store it in the DataCache
+ */
+
+public class DataCache {
+}
---
+++ app/src/main/java/com/bignerdranch/android/familymapclient/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bignerdranch.android.familymapclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import Request.LoginRequest;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
